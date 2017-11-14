5f2055376620696953d0ba079b6ef01454de2ea6
Lin Iris <iris861219@gmail.com>
2017-11-14 10:08:03 +0800

    findViews

--- app/src/main/java/com/iris/guess100/MainActivity.java
+++ app/src/main/java/com/iris/guess100/MainActivity.java
@@ -2,6 +2,10 @@ package com.iris.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void findViews(View view){
+        
+        EditText edNumber = (EditText) findViewById(R.id.number);
+        TextView tvSecert = (TextView) findViewById(R.id.secret);
+        TextView tvInfo = (TextView) findViewById(R.id.info);
+        Button btSend = (Button) findViewById(R.id.send);
     }
 }
